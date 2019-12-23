f5d060e3fcd30daa1c46ed1ec8d2146872389e7b
nidal dahabra <nidal.98@outlook.com>
2019-12-23 22:32:09 +0200

    add menu

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -3,22 +3,76 @@ import 'package:instagram_demo/services/auth_service.dart';
 
 class FeedScreen extends StatefulWidget {
   static final String id = 'feed_screen';
+
   @override
   _FeedScreenState createState() => _FeedScreenState();
 }
+String currentProfilePic = "https://avatars3.githubusercontent.com/u/16825392?s=460&v=4";
+String otherProfilePic = "https://yt3.ggpht.com/-2_2skU9e2Cw/AAAAAAAAAAI/AAAAAAAAAAA/6NpH9G8NWf4/s900-c-k-no-mo-rj-c0xffffff/photo.jpg";
+
 
 class _FeedScreenState extends State<FeedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
-body: Center(
-  child: FlatButton(
-    onPressed: ()=> AuthService.logout(context),
-    child: Text('Logout'),
-  ),
-)
-    );
+        appBar: AppBar(title: Text("appBar"),backgroundColor: Colors.red),
+        drawer: Drawer(
+          child: new ListView(
+            children: <Widget>[
+              new UserAccountsDrawerHeader(
+                accountEmail: new Text("bramvbilsen@gmail.com"),
+                accountName: new Text("Bramvbilsen"),
+                currentAccountPicture: new GestureDetector(
+                  child: new CircleAvatar(
+                    backgroundImage: new NetworkImage(currentProfilePic),
+                  ),
+                  onTap: () => print("This is your current account."),
+                ),
+                otherAccountsPictures: <Widget>[
+                  new GestureDetector(
+                    child: new CircleAvatar(
+                      backgroundImage: new NetworkImage(otherProfilePic),
+                    ),
+//                  onTap: () => switchAccounts(),
+                  ),
+                ],
+                decoration: new BoxDecoration(
+                    image: new DecorationImage(
+                        image: new NetworkImage("https://img00.deviantart.net/35f0/i/2015/018/2/6/low_poly_landscape__the_river_cut_by_bv_designs-d8eib00.jpg"),
+                        fit: BoxFit.fill
+                    )
+                ),
+              ),
+              new ListTile(
+                  title: new Text("Page One"),
+                  trailing: new Icon(Icons.arrow_upward),
+                  onTap: () {
+                    Navigator.of(context).pop();
+//                  Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new Page("First Page")));
+                  }
+              ),
+              new ListTile(
+                  title: new Text("Page Two"),
+                  trailing: new Icon(Icons.arrow_right),
+                  onTap: () {
+                    Navigator.of(context).pop();
+//                  Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new Page("Second Page")));
+                  }
+              ),
+              new Divider(),
+              new ListTile(
+                title: new Text("Cancel"),
+                trailing: new Icon(Icons.cancel),
+                onTap: () => Navigator.pop(context),
+              ),
+            ],
+          ),),
+        backgroundColor: Colors.green,
+        body: Center(
+          child: FlatButton(
+            onPressed: () => AuthService.logout(context),
+            child: Text('Logout'),
+          ),
+        ));
   }
 }
-
